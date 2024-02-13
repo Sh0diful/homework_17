ed428ac5cd723f71d84506082e5b7b469f2eb6df
Sh0diful <sha.dig.sg@gmail.com>
2024-02-12 22:05:37 -0500

    2024/02/12-javascript added

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function toggleFooter() {
     var footer = document.getElementById("footer");
     footer.classList.toggle("hide");
   }
+
+  function toggleBackgroundColor(boxId) {
+    var box = document.getElementById(boxId);
+    // Get the current background color
+    var currentColor = box.style.backgroundColor;
+    
+    // If the background color is white, change it to light blue; otherwise, change it to white
+    if (currentColor === '' || currentColor === 'white') {
+      box.style.backgroundColor = 'lightblue';
+    } else {
+      box.style.backgroundColor = 'white';
+    }
+  }
